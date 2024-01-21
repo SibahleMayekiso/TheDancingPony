7ef95561147589ffd9f4d91af222679957907aa4
SibahleMayekiso <mayekisosibahle@gmail.com>
2024-01-21 15:54:58 +0200

    Initialized Database Models, Configured Project Evnivronment Variables, and Added UserRoutes

--- index.ts
+++ index.ts
@@ -1,11 +1,21 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { initUser } from './models/User';
+import { initDish } from './models/Dish';
+import { initRating } from './models/Rating';
+import userRoutes from './routes/UserRoutes'
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.SERVER_PORT || 3001;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+initUser();
+initDish();
+initRating();
+
+app.use(express.json());
+app.use(userRoutes)
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
