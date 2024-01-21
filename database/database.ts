7b059a74a840d3c4f4f85761d268febd449e214e
SibahleMayekiso <mayekisosibahle@gmail.com>
2024-01-21 15:56:28 +0200

    Modified Database Configuration to Test Connection and Synchronise Models with the Database

--- database/database.ts
+++ database/database.ts
@@ -1,8 +1,24 @@
 import { Sequelize } from "sequelize";
+import dotenv from 'dotenv';
 
-const sequelize = new Sequelize('database', 'username', 'password', {
-    host: 'localhost',
-    dialect: "postgres"
+dotenv.config();
+
+const sequelize = new Sequelize(
+    `postgres://${process.env.POSTGRES_USER!}:${process.env.POSTGRES_PASSWORD!}@localhost:5432/${process.env.POSTGRES_DB!}`
+);
+
+sequelize.authenticate().then(() => {
+    console.log('Connection to database has been established successfully.');
+}).catch(err => {
+    console.error('Unable to connect to database:', err);
 });
+
+sequelize.sync({ alter: true })
+    .then(() => {
+        console.log('All models synced successfully');
+    })
+    .catch((error) => {
+        console.error('An error occurred while syncing models:', error);
+    });
 
 export default sequelize
